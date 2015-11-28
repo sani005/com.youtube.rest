7b92e2f575878e29ba16244efaf248a991ad9a84
Sani005 <anirai786@gmail.com>
2015-11-27 19:14:31 -0700

    post data with jsonobject and jsonarray

--- src/com/youtube/rest/inventory/V3_inventory.java
+++ src/com/youtube/rest/inventory/V3_inventory.java
@@ -1,5 +1,68 @@
 package com.youtube.rest.inventory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.youtube.dao.OracleSani005;
+import com.youtube.util.ToJSON;
+
+import com.youtube.dao.SchemaSani005;
+
+@Path("/v3/inventory")
 public class V3_inventory {
 
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts2(String incomingData) throws Exception {
+		
+		String returnString = null;
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject();
+		SchemaSani005 dao = new SchemaSani005();
+		
+		try {
+			
+			JSONObject partsData = new JSONObject(incomingData);
+			System.out.println("jsonData: " + partsData.toString());
+			
+
+			int http_code = dao.insertIntoPC_PARTS(partsData.optString("PC_PARTS_TITLE"), 
+					partsData.optString("PC_PARTS_CODE"), 
+					partsData.optString("PC_PARTS_MAKER"), 
+					partsData.optString("PC_PARTS_AVAIL"), 
+					partsData.optString("PC_PARTS_DESC"));
+			
+			if( http_code == 200 ) {
+				jsonObject.put("HTTP_CODE", "200");
+				jsonObject.put("MSG", "Item has been entered successfuly, Version 3");
+				returnString = jsonArray.put(jsonObject).toString();
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			System.out.println("return String: " + returnString);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
 }
